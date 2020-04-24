ce2a32ea88b7d8586f71aee3278cf41db113647d
Soldann <guotl321@gmail.com>
2020-04-24 18:50:34 -0400

    basic connect to websocket

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 "use strict";
 
-console.log("hello world");
+const auth = require("./auth.json"); //load tokens using require since they shouldn't be modified
+const https = require("https");
+const ws = require("ws");
+
+https.get("https://discordapp.com/api/gateway", function(res){ //get the WebSocket gateway from Discord
+    var data = "";
+    res.on('data',function(res){data += res;});
+    res.on('end',function(){runBot(JSON.parse(data).url);});
+});
+
+function runBot(gateway){
+    var connection = new ws(gateway);
+
+    connection.on('open',function(){
+        connection.send(JSON.stringify({ //send handshake
+            "op": 2,
+            "d": {
+              "token": auth.discord_token,
+              "properties": {
+                "$os": "linux",
+                "$browser": "nodejs",
+                "$device": "nodejs"
+              }
+            }
+          }));
+    });
+
+    connection.on('message',function(res){
+        console.log(res);
+    });
+}
