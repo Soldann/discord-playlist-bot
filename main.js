3a718345c1aeb92f5593cf062fd9cee04b520eba
Soldann <guotl321@gmail.com>
2020-04-25 19:07:04 -0400

    object oriented refactor

--- main.js
+++ main.js
@@ -7,42 +7,35 @@ const ws = require("ws");
 https.get("https://discordapp.com/api/gateway", function(res){ //get the WebSocket gateway from Discord
     var data = "";
     res.on('data',function(res){data += res;});
-    res.on('end',function(){runBot(JSON.parse(data).url);});
+    res.on('end',function(){
+        var bot = new Bot(JSON.parse(data).url);
+        bot.runBot();
+    });
 });
 
-function runBot(gateway){
-    var connection = new ws(gateway);
-    var sendHeartbeat = false;
-    var heartbeatSender = null;
-    var lastSequenceNum = null;
-
-    function handleMessage(message) {
-        lastSequenceNum = message.s;
-        if (message.op == 10){
-            heartbeatSender = setInterval(function(){
-                connection.send(JSON.stringify({
-                    "op" : 1,
-                    "d" : lastSequenceNum
-                }));
-            },message.d.heartbeat_interval);
-        }
+class Bot {
+    constructor(gateway){
+        this.connection = new ws(gateway);
     }
 
-    connection.on('open',function(){
-        connection.send(JSON.stringify({ //send handshake
-            "op": 2,
-            "d": {
-              "token": auth.discord_token,
-              "properties": {
-                "$os": "linux",
-                "$browser": "nodejs",
-                "$device": "nodejs"
-              }
-            }
-          }));
-    });
+    runBot(){
+        this.connection.on('open',() => {
+            this.connection.send(JSON.stringify({ //send handshake
+                "op": 2,
+                "d": {
+                  "token": auth.discord_token,
+                  "properties": {
+                    "$os": "linux",
+                    "$browser": "nodejs",
+                    "$device": "nodejs"
+                  }
+                }
+              }));
+        });
+    
+        this.connection.on('message',function(res){
+            console.log(JSON.parse(res));
+        });
+    }
 
-    connection.on('message',function(res){
-        handleMessage(JSON.parse(res));
-    });
 }
